de7d37248368f53893347b6ab3cce3df755488be
Pau Escrich <p4u@dabax.net>
2017-03-21 02:08:41 +0100

    Add snippets directory to include small scripts. Include regdbtz snippet
    Signed-off-by: Pau Escrich <p4u@dabax.net>

---
+++ snippets/regdbtz.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+. options.conf
+
+base_feed="$feeds_dir/base"
+[ ! -d $base_feed ] && {
+  echo "$base_feed does not exist. Before applying this snippet you must clone the feeds (option -f of cooker)"
+  exit 1
+}
+
+patch_file="$PWD/$tmp_dir/regdb.patch"
+
+cat >> $patch_file << EOF
+diff --git a/package/kernel/mac80211/files/regdb.txt b/package/kernel/mac80211/files/regdb.txt
+index c4a9b2d..1696011 100644
+--- a/package/kernel/mac80211/files/regdb.txt
++++ b/package/kernel/mac80211/files/regdb.txt
+@@ -1180,8 +1180,8 @@ country TW: DFS-FCC
+ 	(5725 - 5850 @ 80), (30)
+  
+ country TZ:
+-	(2402 - 2482 @ 40), (20)
+-	(5735 - 5835 @ 80), (30)
++	(2402 - 2484 @ 40), (30)
++	(5150 - 5835 @ 80), (30)
+ 
+ # Source:
+ # #914 / 06 Sep 2007: http://www.ucrf.gov.ua/uk/doc/nkrz/1196068874
+EOF
+
+( cd $base_feed && git apply $patch_file )
+
+echo "Patch applied, now you can use the special channel TZ when you deploy a mesh network on International Waters"
