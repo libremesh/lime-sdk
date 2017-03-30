83a0638e96e4976acee4021910bde3fd1c6c6cde
Pau Escrich <p4u@dabax.net>
2017-03-30 06:58:52 +0200

    Snippet to add REMOVE_FILES to ImageBuilders
    It allows to remove files after preparing the firmware
    but before generating the final binary.
    
    Signed-off-by: Pau Escrich <p4u@dabax.net>

---
+++ snippets/patch_ib_remove_files.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+RELEASE="17.01.0"
+
+echo '--- Makefile2	2017-03-30 07:44:01.402299190 +0300
++++ Makefile	2017-03-30 07:43:19.065015108 +0300
+@@ -123,6 +123,14 @@
+ ifneq ($(USER_FILES),)
+ 	$(MAKE) copy_files
+ endif
++ifneq ($(FILES_REMOVE),)
++	@echo
++	@echo Remove useless files
++
++	while read filename; do	\
++	    rm -rfv "$(TARGET_DIR)$$filename"; \
++	done < $(FILES_REMOVE);
++endif
+ 	$(MAKE) package_postinst
+ 	$(MAKE) build_image
+ 	$(MAKE) checksum
+' > tmp/ib_remove_files.patch
+
+[ ! -d $RELEASE ] && echo "Dir $RELEASE not found" && exit 1
+find $RELEASE/ -maxdepth 3 -type d -name 'ib' | while read d; do
+    echo "Patching $d..."
+    patch -p1 $d/Makefile < tmp/ib_remove_files.patch
+done
