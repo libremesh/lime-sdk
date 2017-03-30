fc1fa6bfa81d021e68fa1c54fe31c3e7fc7489f5
Pau Escrich <p4u@dabax.net>
2017-03-30 05:40:13 +0200

    Fix problems with compiling all profiles
      There is not a mechanism to tell IB to keep going if a profile fails
      So the cooking of all target profiles does not work if the firmware
      size is bigger than 4MB. As workaround get the list of possible
      profiles and cook one after the other. To accelerate the process,
      a patch for IB which includes NO_UPDATE=1 (for opkg) has been added.
    
    Signed-off-by: Pau Escrich <p4u@dabax.net>

---
+++ snippets/patch_ib_no_update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Patches all installed ImageBuilders to not opkg update if NO_UPDATE=1"
+RELEASE="17.01.0"
+[ ! -d $RELEASE ] && echo "Dir $RELEASE not found" && exit 1
+find $RELEASE/ -maxdepth 3 -type d -name 'ib' | while read d; do
+    echo "Patching $d..."
+    sed -i s/'\($(OPKG) update || true;\)'/'[ -z \"$(NO_UPDATE)\" ] \&\& \1'/g $d/Makefile
+done
