6b54135e2259b71a013fc1fe4417838cfd0f14b3
Pau Escrich <p4u@dabax.net>
2017-06-12 15:12:56 +0200

    snippets: remove deprecated. Use options.conf to get release number
    Signed-off-by: Pau Escrich <p4u@dabax.net>

--- snippets/patch_ib_no_update.sh
+++ snippets/patch_ib_no_update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+. options.conf
 echo "Patches all installed ImageBuilders to not opkg update if NO_UPDATE=1"
-RELEASE="17.01.1"
+RELEASE="$release"
 [ ! -d $RELEASE ] && echo "Dir $RELEASE not found" && exit 1
 find $RELEASE/ -maxdepth 3 -type d -name 'ib' | while read d; do
     echo "Patching $d..."
