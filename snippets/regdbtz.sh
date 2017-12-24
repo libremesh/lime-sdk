3a790bfae31b22e1e1f11bee51e77b664d48f407
p4u <p4u@dabax.net>
2017-12-24 12:16:25 +0100

    Update regDB patch
    Signed-off-by: p4u <p4u@dabax.net>

--- snippets/regdbtz.sh
+++ snippets/regdbtz.sh
@@ -9,26 +9,23 @@ echo "Patch will be applied to $base_feed"
   exit 1
 }
 
-patch_file="$PWD/$tmp_dir/regdb.patch"
+patch_file="$base_feed/package/firmware/wireless-regdb/patches/600-tz-regd.patch"
 
 cat > $patch_file << EOF
-diff --git a/package/kernel/mac80211/files/regdb.txt b/package/kernel/mac80211/files/regdb.txt
-index c4a9b2d15f..ec2c21543a 100644
---- a/package/kernel/mac80211/files/regdb.txt
-+++ b/package/kernel/mac80211/files/regdb.txt
-@@ -1180,8 +1180,8 @@ country TW: DFS-FCC
- 	(5725 - 5850 @ 80), (30)
+--- a/db.txt
++++ b/db.txt
+@@ -1209,8 +1209,8 @@ country TW: DFS-FCC
+ 	(57000 - 66000 @ 2160), (40)
   
  country TZ:
 -	(2402 - 2482 @ 40), (20)
 -	(5735 - 5835 @ 80), (30)
-+       (2402 - 2484 @ 40), (30)
-+       (5150 - 5835 @ 80), (30)
++	(2402 - 2482 @ 40), (30)
++	(5150 - 5835 @ 80), (30)
  
  # Source:
  # #914 / 06 Sep 2007: http://www.ucrf.gov.ua/uk/doc/nkrz/1196068874
 EOF
 
-( cd $base_feed && git apply $patch_file && {
-  echo "Patch applied, now you can use the special channel TZ when you deploy a mesh network on International Waters"
-} || echo "Patch does not apply, maybe it is already applied orLEDE source has changed" )
+echo "Patch applied, now you can use the special channel TZ when you deploy a mesh network on International Waters"
+
