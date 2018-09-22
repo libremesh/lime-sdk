f0bc937753083df6ef4ddf990fc76f26b34f6dcf
Marcos Gutierrez <gmarcos87@gmail.com>
2018-09-22 14:24:36 -0300

    In 18.06.1 regdb.txt just became a package of its own

--- snippets/regdbtz.sh
+++ snippets/regdbtz.sh
@@ -9,9 +9,10 @@ echo "Patch will be applied to $base_feed"
   exit 1
 }
 
-patch_file="$base_feed/package/firmware/wireless-regdb/patches/600-tz-regd.patch"
+patch_file="$PWD/$tmp_dir/999-tz_hack.patch"
 
 cat > $patch_file << EOF
+
 --- a/db.txt
 +++ b/db.txt
 @@ -1209,8 +1209,8 @@ country TW: DFS-FCC
@@ -20,12 +21,13 @@ cat > $patch_file << EOF
  country TZ:
 -	(2402 - 2482 @ 40), (20)
 -	(5735 - 5835 @ 80), (30)
-+	(2402 - 2482 @ 40), (30)
++	(2402 - 2484 @ 40), (30)
 +	(5150 - 5835 @ 80), (30)
  
  # Source:
  # #914 / 06 Sep 2007: http://www.ucrf.gov.ua/uk/doc/nkrz/1196068874
 EOF
 
-echo "Patch applied, now you can use the special channel TZ when you deploy a mesh network on International Waters"
-
+( cd $base_feed && mv $patch_file package/firmware/wireless-regdb/patches/ && {
+  echo "Patch copied, now you can use the special channel TZ when you deploy a mesh network on International Waters"
+} || echo "Patch copy error, maybe it is already applied or OpenWRT source has changed" )
